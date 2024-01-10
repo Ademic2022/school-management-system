62f3d82a468d39e5914d4164c2faabdafefe2623
“Ademic” <adeeyomichael77@gmail.com>
2024-01-10 02:47:55 +0100

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # This script is used to build and set up the Django project.
 
 # Install MySQL development library
+apt-get update
 apt-get install -y libmysqlclient-dev
 
 # Create and activate virtual environment
@@ -10,23 +11,23 @@ python -m venv venv
 source venv/bin/activate
 
 # Install project dependencies
-if ! python -m pip install -r requirements.txt; then
+if ! python -m pip install -r /vercel/path0/requirements.txt; then
   echo "Error: Failed to install dependencies."
   exit 1
 fi
 
 # Make database migrations
-if ! python manage.py makemigrations core; then
+if ! python /vercel/path0/manage.py makemigrations core; then
   echo "Error: Failed to make migrations for core app."
   exit 1
 fi
 
-if ! python manage.py makemigrations users; then
+if ! python /vercel/path0/manage.py makemigrations users; then
   echo "Error: Failed to make migrations for users app."
   exit 1
 fi
 
-if ! python manage.py migrate; then
+if ! python /vercel/path0/manage.py migrate; then
   echo "Error: Failed to apply migrations."
   exit 1
 fi
