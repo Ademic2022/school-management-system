fb226a61c30b48dc4ebe7f7ebdce9ca803c3c34e
ademic2022 <adeeyomichael77@gmail.com>
2024-01-15 16:40:43 +0100

    feat: add new host

--- school_management_system/settings.py
+++ school_management_system/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "school-management-system-alpha.vercel.app",
-    "school-management-system-dlbe49zs9-ademics-projects.vercel.app",
+    "school-management-system-d8vr6ya5k-ademics-projects.vercel.app",
 ]
 # ALLOWED_HOSTS = ["*"]
 
